8dbe8afcfdc29c6dd1402715e61afa52ad265e6a
will <wjandrews672@gmail.com>
2021-02-26 17:16:38 -0800

    finished calc visuals and got screen to display

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let screen = document.querySelector('p');
+const numberButtons = document.querySelectorAll('[data-number]');
+const opButtons = document.querySelectorAll('[data-operator]');
+const equalsButton= document.querySelectorAll('[data-equals]');
+// const clearButton
+// const deleteButton
+
+let displayValuex = '';
+let displayValuey = '';
+let operateValue = '';
+
+
 function add(x, y) {
     return x + y;
 }
@@ -14,6 +26,44 @@ function multiply(x, y) {
     return x * y;
 }
 
-function operate(op, x, y) {
-    return op(x, y);
-}
+// function operate(x, operateValue, y) {
+//     return operateValue(x, y);
+//     //why error code
+// }
+
+// const displayValue = function() {
+
+// }
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // if (screen = '') {
+            displayValuex +=button.textContent;
+            screen.textContent += button.textContent;
+        // } else {
+            // displayValuey +=button.textContent;
+            // screen.textContent += button.textContent;
+        // }
+    });
+});
+
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.textContent == 'x') {
+            operateValue = multiply;
+        } else  if (button.textContent == '/') {
+            operateValue = divide;
+        } else  if (button.textContent == '+') {
+            operateValue = add;
+        } else  if (button.textContent == '-') {
+            operateValue = subtract;
+        }
+        //run operate function on click
+        //x = display value, then y equal display value
+    console.log(operateValue)
+    });
+});
+
+// equalsButton.addEventListener('click', operate(displayValuex, operateValue, displayValuey));
+//clearButton.addEventListener
+//deleteButton.addEventListener
